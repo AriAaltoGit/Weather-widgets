df5130764d97f1087155bfe53356bff8862fdf43
Ari <ari.orre@aalto.fi>
2018-02-18 00:50:30 +0200

    Added observable for weather component and handle sync location data with observable.

--- app/weather-widget/component/weather.component.ts
+++ app/weather-widget/component/weather.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { WeatherService } from '../service/weather.service';
 
+import {Weather} from '../model/weather';
+
 @Component({
     moduleId: module.id,
     selector: 'weather-widget',
@@ -10,11 +12,18 @@ import { WeatherService } from '../service/weather.service';
     providers: [WeatherService]
 })
 export class WeatherComponent {
+    pos: Position;
+    weatherData = new Weather(null,null,null,null,null);  //Initialize before
+
     constructor(private service: WeatherService) {
-        this.service.getCurrentLocation();
-        this.service.getCurrentWeather(0, 0)
-            .subscribe(weather => console.log(weather),
-                err => console.error(err));
+        this.service.getCurrentLocation()
+            .subscribe(position => {
+                this.pos = position
+                this.service.getCurrentWeather(this.pos.coords.latitude, this.pos.coords.latitude)
+                    .subscribe(weather => console.log(weather),
+                        err => console.error(err));
+            },
+            err => console.error(err));
     }
 }
 
--- app/weather-widget/service/weather.service.ts
+++ app/weather-widget/service/weather.service.ts
@@ -1,39 +1,40 @@
-import {Injectable} from '@angular/core';
-import {Jsonp} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-import {FORECAST_KEY, FORECAST_ROOT} from '../constants/constants';
+import { FORECAST_KEY, FORECAST_ROOT } from '../constants/constants';
 
 @Injectable()
 export class WeatherService {
 
-    constructor(private jsonp: Jsonp ) {
+    constructor(private jsonp: Jsonp) { }
 
+    getCurrentLocation(): Observable<any>{
+    if (navigator.geolocation) {
+        return Observable.create(observer => {
+            navigator.geolocation.getCurrentPosition(pos => {
+                observer.next(pos)
+            }),
+                err => {
+                    return Observable.throw(err);
+                }
+        });
+    } else {
+        return Observable.throw("Geolocation is not available")
     }
-    getCurrentLocation(): [number,number]{
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(pos=>{
-                console.log("Position: ", pos.coords.latitude, ",", pos.coords.longitude);
-                return [pos.coords.latitude, pos.coords.longitude];
-            },
-            err=> console.error("Unable to get the position - ", err));
-        }else {
-            console.error("Geolocation is not available");
-            return[0,0]
-        }
-    }
+}
 
-    getCurrentWeather(lat: number, long: number): Observable<any> {
-        const url = FORECAST_ROOT + FORECAST_KEY + "/" + lat + "," + long;
-        const queryParams = "?callback=JSONP_CALLBACK";
+getCurrentWeather(lat: number, long: number): Observable < any > {
+    const url = FORECAST_ROOT + FORECAST_KEY + "/" + lat + "," + long;
+    const queryParams = "?callback=JSONP_CALLBACK";
 
-        return this.jsonp.get(url + queryParams)
+    return this.jsonp.get(url + queryParams)
         .map(data => data.json())
         .catch(err => {
             console.error("Unable to get weather data - ", err);
             return Observable.throw(err.json())
-        })
-    }
+        });
+}
 }
